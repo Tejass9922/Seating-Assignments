ed2262d05a4615fa2f0e0725164c8f6675736069
Tejas Sameera <sameera.tejas5@gmail.com>
2021-08-24 22:07:35 -0400

    deploy 2.0

--- backend.py
+++ backend.py
@@ -4,40 +4,61 @@ import csv
 import sys
 app = Flask(__name__)
 app.secret_key= "tejas_is_very_cool"
-
-@app.route('/')
+SHA_SECRET_KEY = 'b37e50cedcd3e3f1ff64f4afc0422084ae694253cf399326868e07a35f4a45fb'
+@app.route('/'+ SHA_SECRET_KEY)
 def home():
     session.clear()
     last_names = list(fetch_last_names())
     print(last_names)
     return render_template('login.html', last_names = json.dumps(last_names))
 
-@app.route('/seating/success')
+@app.route('/seating/success/' + SHA_SECRET_KEY)
 def success():
     if "table_number" in session and "user" in session:
         number = int(session["table_number"])
         name = str(session["user"])
+        party_indicator = int(session['party_indicator'])
         session.pop("user")
         session.pop("table_number")
-        return render_template("homepage.html",first_name=name,table_number=number)
-       #return 'welcome %s your table number is: %d' % (name,number)
+        session.pop("party_indicator")
+        return render_template("homepage.html",first_name=name,table_number=number,party_indicator=party_indicator)
+     
     else:
        return redirect(url_for('home'))
 
-@app.route('/seating/fail')
+@app.route('/seating/fail/' + SHA_SECRET_KEY)
 def failure():
     if "user" in session:
         name = str(session["user"])
         session.pop("user")
-     #   return render_template("homepage.html")
         return render_template("altHomepage.html",first_name=name)
     else:
         return redirect(url_for('home'))
 
-@app.route('/seating/chartView')
+@app.route('/seating/chartView/' + SHA_SECRET_KEY)
 def chart_view():
-    (headers,table_data) = fetch_table_format()
-    return render_template("seatingAssignment.html",headers=headers,table_data=table_data)
+        (headers,table_data) = fetch_table_format()
+        return render_template("seatingAssignment.html",headers=headers,table_data=table_data)
+   
+
+@app.route('/seating/partyInfo/' + SHA_SECRET_KEY,methods = ['POST', 'GET'])
+def party_info():
+    if request.method == 'POST':
+        party_indicator = int(request.form['partyIndicator'])
+        name = str(request.form['name'])
+        (headers,data) = query_party_assignment(party_indicator)
+        return render_template('partyData.html',headers=headers,table_data=data,name=name)
+
+@app.route('/seating/tableInfo/' + SHA_SECRET_KEY,methods = ['POST', 'GET'])
+def table_info():
+    if request.method == 'POST':
+        table =  int(request.form['tableNumber'])
+        name = str(request.form['name'])
+        (headers,data) = query_table_data(table)
+       # print(data)
+        return render_template('tableData.html',headers = headers, table_data = data,table_number=table, name =name)
+    else:
+        return redirect(url_for('home'))
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
@@ -46,19 +67,40 @@ def login():
         last = str(request.form['lastname'])
         print((first,last))
         name = (first,last)
-        table_number = find_table(name)
-        session["user"] = first
+        (formatted_name,table_number,party_indicator) = find_table(name)
+        session["user"] = first if formatted_name == 'FAIL_CASE' else formatted_name
         if table_number == -1:
             return redirect(url_for('failure'))
 
         session["table_number"] = (table_number)
-        
+        session["party_indicator"] = party_indicator
         print(session["table_number"])
         return redirect(url_for('success'))
     else: 
         return render_template('login.html')
      
-       
+
+def query_table_data(table_number):
+    headers = ("First Name", "Last Name")
+    csv_file = csv.reader(open('test.csv', "r"), delimiter=",")
+    data = []
+    for row in csv_file:
+        if int(row[2]) == table_number:
+            data.append([row[0],row[1]])
+   # print(data)
+    return (headers,data)
+
+def query_party_assignment(party_identifier):
+    headers = ("First Name", "Last Name", "Table Number")
+    data = []
+    csv_file = csv.reader(open('test.csv', "r"), delimiter=",")
+    for row in csv_file:
+        if int(row[3]) == party_identifier:
+             data.append([row[0],row[1],row[2]])
+    print("party identifier %d" % party_identifier)
+    print(data)
+    return (headers,data)
+
 
 def fetch_last_names():
     csv_file = csv.reader(open('test.csv', "r"), delimiter=",")
@@ -78,9 +120,9 @@ def find_table(name):
 
     for row in csv_file:  
         if row[0].lower() == first.lower() and row[1].lower() == last.lower():
-            return row[2]
+            return (row[0],row[2],row[3])
 
-    return -1
+    return ('FAIL_CASE',-1, -1)
 
 def fetch_table_format():
     csv_file = csv.reader(open('test.csv', "r"), delimiter=",")
